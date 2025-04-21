7df6efc40f23e427dec54b58612054d4da8a2568
Valentin Afonso <valentinafonso22@gmail.com>
2025-04-21 11:46:23 +0200

    🔒️fix: authorize cors for local dev

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ import { logger } from "hono/logger";
 import { routes } from "./routes";
 import { authMiddleware } from "./middleware/auth";
 import { EnvBindings } from "./types/env";
+import { cors } from "hono/cors";
 
 const app = new Hono<{ Bindings: EnvBindings }>();
 
 // Middleware
 app.use("/api/*", logger());
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use("/api/*", authMiddleware);
 
 // Routes handling
