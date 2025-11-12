6644822cba3628164aad1784dbba29f6bd674db2
Valentin Afonso <valentinafonso22@gmail.com>
2025-11-12 22:28:08 +0100

    feat: add coins details route api

--- src/routes/crypto.ts
+++ src/routes/crypto.ts
@@ -8,11 +8,14 @@ const cryptoRoutes = new Hono<{ Bindings: EnvBindings }>();
 cryptoRoutes.get(`/coins/supported_vs_currencies`, async (c) => {
   return proxy(getCoingeckoUrl(c, "simple/supported_vs_currencies"));
 });
+
+// Get all coins markets for table view
 cryptoRoutes.get(`/coins/markets`, async (c) => {
   const { vs_currency, sparkline } = c.req.query();
   return proxy(getCoingeckoUrl(c, "coins/markets", { vs_currency, sparkline }));
 });
 
+// Get all markets for a specific coin for teaser view
 cryptoRoutes.get(`/coins/:symbol/markets`, async (c) => {
   const symbol = c.req.param("symbol");
   const { vs_currency, days } = c.req.query();
@@ -20,4 +23,12 @@ cryptoRoutes.get(`/coins/:symbol/markets`, async (c) => {
     getCoingeckoUrl(c, `coins/${symbol}/market_chart`, { vs_currency, days })
   );
 });
+
+// Get a specific coin for detail page
+cryptoRoutes.get(`/coins/:id`, async (c) => {
+  const id = c.req.param("id");
+  const { vs_currency } = c.req.query();
+  return proxy(getCoingeckoUrl(c, `coins/${id}`, { vs_currency }));
+});
+
 export { cryptoRoutes };
